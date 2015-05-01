05958a1a10b3e64dbb3b1778983dab4176953b55
Kelly O'Briant <kellyobriant@gmail.com>
2015-05-01 00:24:26 -0400

    blocmarks sorted by topic in topic view index

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   post :incoming, to: 'incoming#create'
 
   get 'blocmarks', to: 'bookmarks#index'
+  get 'topics', to: 'topics#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
